cd7c0294fec1d107cf2c8412d957b8ae538f46b2
Ray Zhang <peifeng2005@gmail.com>
2020-06-11 20:19:54 -0700

    it kinda works

--- ClangCast.cpp
+++ ClangCast.cpp
@@ -1,28 +1,84 @@
-// Declares clang::SyntaxOnlyAction.
-#include "clang/Frontend/FrontendActions.h"
-#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/Tooling.h"
-// Declares llvm::cl::extrahelp.
-#include "llvm/Support/CommandLine.h"
+#include "clang/Tooling/CommonOptionsParser.h"
 
-using namespace clang::tooling;
-using namespace llvm;
+using namespace clang;
 
-// Apply a custom category to all command-line options so that they are the
-// only ones displayed.
-static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+static llvm::cl::OptionCategory ClangCastCategory("clang-cast options");
 
-// CommonOptionsParser declares HelpMessage with a description of the common
-// command-line options related to the compilation database and input files.
-// It's nice to have this help message in all tools.
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+class FindCStyleCastVisitor
+        : public RecursiveASTVisitor<FindCStyleCastVisitor> {
+public:
+    explicit FindCStyleCastVisitor(ASTContext *Context)
+            : Context(Context) {}
 
-// A help message for this specific tool can be added afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...\n");
+    bool VisitCStyleCastExpr(CStyleCastExpr *CastExpression) {
+        QualType CastType = CastExpression->getTypeAsWritten();
+        QualType CanonicalCastType = CastType.getCanonicalType();
+        Expr* SubExpression = CastExpression->getSubExprAsWritten();
+        SubExpression->dump();
+        if (!SubExpression) {
+            llvm::outs() << "The expression is invalid.\n";
+        }
+        QualType SubExpressionType = SubExpression->getType();
+        QualType SubExpressionCanonicalType = SubExpressionType.getCanonicalType();
+
+        auto& SourceManager = Context->getSourceManager();
+        SourceLocation Loc = SourceManager.getSpellingLoc(CastExpression->getBeginLoc());
+        FullSourceLoc DiagnosticLocation = FullSourceLoc(Loc, SourceManager);
+        if (Loc.isInvalid()) {
+            llvm::outs() << "Loc invalid" << "\n";
+            return false;
+        }
+        const FileEntry *FileEntry =
+                SourceManager.getFileEntryForID(SourceManager.getFileID(Loc));
+        if (!FileEntry) {
+            llvm::outs() << "File entry invalid" << "\n";
+            return false;
+        }
+        llvm::outs() << "Found c-style cast expression in file " <<
+            FileEntry->getName() << " on line " << DiagnosticLocation.getSpellingLineNumber() <<
+            " casting to type " << CanonicalCastType.getAsString() << " from type " <<
+            SubExpressionCanonicalType.getAsString() << ".\n";
+        return true;
+    }
+
+private:
+    ASTContext *Context;
+};
+
+class FindCStyleCastConsumer : public clang::ASTConsumer {
+public:
+    explicit FindCStyleCastConsumer(ASTContext *Context)
+            : Visitor(Context) {}
+
+    virtual void HandleTranslationUnit(clang::ASTContext &Context) {
+        Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+    }
+private:
+    FindCStyleCastVisitor Visitor;
+};
+
+class FindCStyleCastAction : public clang::ASTFrontendAction {
+public:
+    virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+            clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
+        return std::unique_ptr<clang::ASTConsumer>(
+                new FindCStyleCastConsumer(&Compiler.getASTContext()));
+    }
+};
 
 int main(int argc, const char **argv) {
-  CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
-  ClangTool Tool(OptionsParser.getCompilations(),
-                 OptionsParser.getSourcePathList());
-  return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
+    // parse the command-line args passed to your code
+    tooling::CommonOptionsParser op(argc, argv, ClangCastCategory);
+    // create a new Clang Tool instance (a LibTooling environment)
+    tooling::ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+
+    // run the Clang Tool, creating a new FrontendAction (explained below)
+    std::unique_ptr<tooling::FrontendActionFactory> action_ptr =
+        tooling::newFrontendActionFactory<FindCStyleCastAction>();
+    int result = Tool.run(action_ptr.get());
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # clang-cast
 A Clang libtool project that changes all C-style casts to C++ static casts.
+
+To build this in clang's environment, do the following:
+
+```shell script
+cmake -G Ninja ../llvm -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra" -DLLVM_BUILD_TESTS=ON -DCMAKE_CXX_STANDARD=20
+ninja clang-cast
+```
