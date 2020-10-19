5f0b04fb90ed5de5b86d080a8e73a71287d0abcf
Ray Zhang <peifeng2005@gmail.com>
2020-10-18 21:49:48 -0700

    add license stuff

--- tests/ClangChangeQualifierTestCases.h
+++ tests/ClangChangeQualifierTestCases.h
@@ -1,3 +1,10 @@
+//===-- ClangChangeQualifierTestCases.h - clang-cast ------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 #ifndef LLVM_PROJECT_CLANGCHANGEQUALIFIERTESTCASES_H
 #define LLVM_PROJECT_CLANGCHANGEQUALIFIERTESTCASES_H
 
--- tests/ClangFunctionPtrTestCases.h
+++ tests/ClangFunctionPtrTestCases.h
@@ -1,3 +1,10 @@
+//===-- ClangFunctionPtrTestCases.h - clang-cast ----------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 #ifndef LLVM_PROJECT_CLANGFUNCTIONPTRTESTCASES_H
 #define LLVM_PROJECT_CLANGFUNCTIONPTRTESTCASES_H
 
